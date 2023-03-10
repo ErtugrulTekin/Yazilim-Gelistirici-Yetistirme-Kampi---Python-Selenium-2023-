a8ff4f82612b850151dcf1408d553d48b41f95bc
Ertugrul Tekin <ertugrultekin98@gmail.com>
2023-03-10 23:35:26 +0300

    Week1

---
+++ Week1/main.py
@@ -0,0 +1,20 @@
+print("Hello world")
+baslik = "Taşıt Kredisi"
+print(baslik)
+#String => metinsel ifade
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+
+vade = 36
+ekVade= 6     
+vade2 = "36"
+
+aylikOdeme = 200.50
+
+
+yukselisteMi = True,False
+
+print(5+5)
+print(vade+12)
+print(vade + ekVade)
+
